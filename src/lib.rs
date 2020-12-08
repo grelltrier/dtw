f0a4d3de33ec27fb73e1a8a53f0ab9b416706185
pentamassiv <pentamassiv@posteo.de>
2020-12-08 13:59:02 +0100

    Moved DTW implementation to modules

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use ndarray::Data;
 use ndarray_stats::DeviationExt;
 use num_traits::{cast::ToPrimitive, sign::Signed};
 
+pub mod naive;
+pub mod ucr;
+
 // Calculate the L2 distance (euclidian distance) for a vector
 pub fn l2_dist<T, A, D>(a: &ArrayBase<T, D>, b: &ArrayBase<T, D>) -> f64
 where
@@ -12,59 +15,6 @@ where
     D: Dimension,
 {
     a.l2_dist(b).unwrap()
-}
-
-/// Calculate the similarity of two sequences of vectors of n components with a naive implementation of DTW (no pruning or other optimizations)
-/// Inspired by https://www.codesuji.com/2020/11/05/Rust-and-Dynamic-Time-Warping/
-pub fn dtw_naive<F, T, A, D>(
-    a: &[ArrayBase<T, D>],
-    b: &[ArrayBase<T, D>],
-    norm_func: F,
-    debug: bool,
-) -> f64
-where
-    F: Fn(&ArrayBase<T, D>, &ArrayBase<T, D>) -> f64,
-    T: Data<Elem = A>,
-    A: AddAssign + Clone + Signed + ToPrimitive,
-    D: Dimension,
-{
-    // Init similarity matrix
-    let a_len = a.len() + 1;
-    let b_len = b.len() + 1;
-    let mut similarity_mtrx = Array::<f64, Ix2>::from_elem((a_len, b_len), f64::MAX);
-    similarity_mtrx[[0, 0]] = 0.;
-
-    // Calculate similarity matrix
-    for i in 1..a_len {
-        for j in 1..b_len {
-            let cost = norm_func(&a[i - 1], &b[j - 1]);
-            similarity_mtrx[[i, j]] = cost
-                + f64::min(
-                    f64::min(similarity_mtrx[[i - 1, j]], similarity_mtrx[[i, j - 1]]),
-                    similarity_mtrx[[i - 1, j - 1]],
-                );
-        }
-    }
-
-    // Print similarity matrix
-    if debug {
-        for row_no in 0..a_len {
-            for cell_no in 0..b_len {
-                print!(
-                    "{:5} ",
-                    (if similarity_mtrx[[row_no, cell_no]] == f64::MAX {
-                        String::from("-")
-                    } else {
-                        format!("{:.*}", 2, similarity_mtrx[[row_no, cell_no]])
-                    })
-                );
-            }
-            println!();
-        }
-    }
-
-    // Return final cost
-    similarity_mtrx[[a_len - 1, b_len - 1]]
 }
 
 pub mod utilities {
@@ -97,7 +47,7 @@ mod tests {
     #[test]
     fn cost_function() {
         let (series_1, series_2) = utilities::make_test_series();
-        let cost = dtw_naive(&series_1, &series_2, l2_dist, false);
+        let cost = naive::dtw(&series_1, &series_2, l2_dist, false);
         assert!((0.55 - cost).abs() < 0.000000000001);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use dtw::*;
 
 fn main() {
     let (series_1, series_2) = utilities::make_test_series();
-    let cost = dtw_naive(&series_1, &series_2, l2_dist, true);
+    let cost = naive::dtw(&series_1, &series_2, l2_dist, true);
     println!("cost: {}", cost);
 }
