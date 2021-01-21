c51e6eb4ecc93848cb942ef2eab4449fa0bdacf0
pentamassiv <pentamassiv@posteo.de>
2021-01-21 23:51:50 +0100

    Fixed tests and added comparison naive/ucr

--- src/naive.rs
+++ src/naive.rs
@@ -1,36 +1,38 @@
 use super::*;
 
-/// Calculate the similarity of two sequences of vectors of n components with a naive implementation of DTW (no pruning or other optimizations)
-pub fn dtw<F, T>(a: &[T], b: &[T], cost_fn: F, debug: bool) -> f64
+/// Calculate the DTW cost of two sequences of vectors of n components with a naive implementation (no pruning or other optimizations)
+pub fn dtw<F, T>(series_a: &[T], series_b: &[T], cost_fn: F, debug: bool) -> f64
 where
     F: Fn(&T, &T) -> f64,
+    T: std::fmt::Debug,
 {
-    // Init similarity matrix
-    let mut similarity_mtrx = Array::<f64, Ix2>::from_elem((a.len() + 1, b.len() + 1), f64::MAX);
-    similarity_mtrx[[0, 0]] = 0.;
+    // Init cost matrix
+    let mut cost_mtrx =
+        Array::<f64, Ix2>::from_elem((series_a.len() + 1, series_b.len() + 1), f64::INFINITY);
+    cost_mtrx[[0, 0]] = 0.;
 
-    // Calculate similarity matrix
-    for i in 1..=a.len() {
-        for j in 1..=b.len() {
-            let cost = cost_fn(&a[i - 1], &b[j - 1]);
-            similarity_mtrx[[i, j]] = cost
+    // Calculate cost matrix
+    for i in 1..=series_a.len() {
+        for j in 1..=series_b.len() {
+            let cost = cost_fn(&series_a[i - 1], &series_b[j - 1]);
+            cost_mtrx[[i, j]] = cost
                 + f64::min(
-                    f64::min(similarity_mtrx[[i - 1, j]], similarity_mtrx[[i, j - 1]]),
-                    similarity_mtrx[[i - 1, j - 1]],
+                    f64::min(cost_mtrx[[i - 1, j]], cost_mtrx[[i, j - 1]]),
+                    cost_mtrx[[i - 1, j - 1]],
                 );
         }
     }
 
-    // Print similarity matrix
+    // Print cost matrix
     if debug {
-        for row_no in 0..a.len() + 1 {
-            for cell_no in 0..b.len() + 1 {
+        for row_no in 0..series_a.len() + 1 {
+            for cell_no in 0..series_b.len() + 1 {
                 print!(
                     "{:5} ",
-                    (if similarity_mtrx[[row_no, cell_no]] == f64::MAX {
+                    (if cost_mtrx[[row_no, cell_no]] == f64::MAX {
                         String::from("-")
                     } else {
-                        format!("{:.*}", 2, similarity_mtrx[[row_no, cell_no]])
+                        format!("{:.*}", 2, cost_mtrx[[row_no, cell_no]])
                     })
                 );
             }
@@ -39,5 +41,5 @@ where
     }
 
     // Return final cost
-    similarity_mtrx[[a.len(), b.len()]]
+    cost_mtrx[[series_a.len(), series_b.len()]]
 }
